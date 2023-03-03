f1323294258888a8b899fe54f8e7c8d03a981ed5
Jacob Lauxman <jlauxman@gmail.com>
2023-03-03 12:13:49 -0700

    pipfile installed, setup for seed creation

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,5 @@
 from .db import db
 from .user import User
 from .db import environment, SCHEMA
+from .coffee import Coffee
+from .post import Post
---
+++ app/seeds/coffees.py
@@ -0,0 +1,9 @@
+from app.models import db, Coffee, environment, SCHEMA
+
+def seed_coffees():
+    pass
+    # coffee1 = Coffee(
+    #     name='Classic',
+    #     year='2023',
+    #     caffeine_percentage=
+    # )
